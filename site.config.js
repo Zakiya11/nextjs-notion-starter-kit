385036e56eb3ec41ea91c51286717ffe301fe0de
ZakiyaGeorge <54877767+Zakiya11@users.noreply.github.com>
2021-04-21 17:50:47 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Zakiya George',
+  domain: 'aboutme.it',
+  author: 'Zakiya George',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: '',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'Zakiya11',
+  linkedin: 'zakiyageo',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
